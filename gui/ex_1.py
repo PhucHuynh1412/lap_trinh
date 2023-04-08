7c270c931b1f10a9e5f977f1caffef20e0cc3577
PhucHuynh1412 <hltphuc@gmail.com>
2023-04-09 04:55:12 +0700

    test

---
+++ gui/ex_1.py
@@ -0,0 +1,13 @@
+import tkinter as tk 
+
+greeting = tk.Label(text = "Python rocks!").pack()
+
+tk.mainloop()
+
+"""
+window = tk.Tk()
+lbl = Label(text = "Python rocks!")
+lbl.pack()
+window.mainloop()
+
+"""
--- gui/login.py
+++ gui/login.py
@@ -4,13 +4,14 @@ def xin_chao():
     lbl.configure(text='Hello Phúc')
 
 window = Tk()
-window.title('App Hello')
+window.title('Login')
 
 lbl = Label(window, text='Hello everybody')
 lbl.grid(column=0, row=0)
 
+
 btn = Button(text='Click Me', command=xin_chao)
-btn.grid(column=0, row=1)
+btn.grid(column=1, row=0)
 
 window.mainloop()
 
