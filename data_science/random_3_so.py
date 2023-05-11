853e95bec75c4adfe28b2412cae8db6855e2ce07
PhucHuynh1412 <hltphuc@gmail.com>
2023-05-11 21:51:00 +0700

    update datascience

--- data_science/random_3_so.py
+++
@@ -1,9 +0,0 @@
-import random
-
-data_num = [0,1,2,3,4,5,6,7,8,9]
-so_dau = random.choice(data_num)
-so_giua = random.choice(data_num)
-so_cuoi = random.choice(data_num)
-
-print(so_dau,so_giua,so_cuoi)
-
