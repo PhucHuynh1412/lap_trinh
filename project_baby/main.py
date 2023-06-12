fcff534d94024e39337712064e6a3588ae45499f
PhucHuynh1412 <hltphuc@gmail.com>
2023-06-12 11:49:40 +0700

    udpate them phan project baby

--- project_baby/main.py
+++ project_baby/main.py
@@ -1,3 +1,4 @@
+import sys 
 import speech_recognition as sr 
 import os
 from playsound import playsound 
@@ -12,7 +13,7 @@ def read_data():
             line = line.split('\t')
             data.append(line)
     return data 
-
+ 
 def speak(text):
     co = gTTS(text, lang='vi')
     co.save('co.mp3')
@@ -32,23 +33,40 @@ def listen():
             time.sleep(1)
             speak("Cô nghe không rõ con nói lại nhen!")
             listen()
+def befor_answer():
+    text = "Ai sẽ trả lời câu hỏi của cô nè! Mira , Misu hay Ngọc Diệp"
+    speak(text)
+
 
 def check_answer(text,dap_an):
-    pass 
-
+    if dap_an in text:
+        text = 'Con trả lời đúng rồi! Giỏi!'
+        speak(text)
+    else:
+        text = 'Con trả lời chưa đúng!'
+        speak(text)
 
 def read_question(text):
+    text_1 = 'Cô bắt đầu đọc câu hỏi , các con nghe kĩ nhen!'
+    speak(text_1)
+    time.sleep(1)
     speak(text)
 
 def listen_answer():
+    time.sleep(1)
+    text = 'Con biết đó là con gì không ? trả lời cô nghe nào ...'
+    speak(text)
     text = listen()
     return text
 
 def program():
     data = read_data()
     for idx, line in enumerate(data):
-        question = f"Câu {idx} là: {line[0]}" 
-        speak(question)
+        question = f"Câu {idx+1} là: {line[0]}" 
+        rs = f"{line[0][1]}"
+        read_question(question)
+        text = listen_answer()
+        check_answer(text,rs)
 
 def main():
     program()
---
+++ project_baby/test_speed_audio.py
@@ -0,0 +1,5 @@
+from pydub import AudioSegment 
+from pydub.playback import play 
+
+def slow_down_audio(duong_dan, toc_do):
+    pass
