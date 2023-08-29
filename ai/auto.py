9f90e1033b2f85d87d899f65951ff0ff9e65e763
PhucHuynh1412 <hltphuc@gmail.com>
2023-08-29 14:10:54 +0700

    update ngay 29/8

--- ai/auto.py
+++
@@ -1,5 +0,0 @@
-import pyautogui as pa 
-
-vi_tri = pa.position()
-print(vi_tri)
-
--- vat_ly/phuong_trinh_dao_dong.py
+++
@@ -1,8 +0,0 @@
-import sympy as sp 
-import pyinputplus as pin 
-
-A = pin.inputFloat('Nhập giá trị biên độ: ')
-w = pin.inputFloat('Nhập giá trị của tần số góc: ')
-phi = pin.inputFloat('Nhập giá trị pha ban đầu: ')
-
-x = A*sp.cos(w*t + phi)
