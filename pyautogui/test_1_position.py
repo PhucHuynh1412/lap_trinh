4330a5866a47b5cff23f354a985f78a1e8f016b2
PhucHuynh1412 <hltphuc@gmail.com>
2023-08-29 17:04:23 +0700

    update lai thu muc lap trinh

--- pyautogui/test_1_position.py
+++
@@ -1,9 +0,0 @@
-import pyautogui as pag 
-
-vi_tri = pag.position()
-print(f"Vị trí của con chuột hiện tại là: {vi_tri}")
-
-kich_thuoc_man_hinh = pag.size()
-print(f"Kích thước màn hình là : {kich_thuoc_man_hinh}")
-
-
