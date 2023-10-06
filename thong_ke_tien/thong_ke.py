993d360f954b48dfdc37233c23a2a629c8df9eaf
PhucHuynh1412 <hltphuc@gmail.com>
2023-10-06 10:03:17 +0700

    update thong ke

--- thong_ke_tien/thong_ke.py
+++ thong_ke_tien/thong_ke.py
@@ -1,15 +1,23 @@
 #TODO: Thu vien
 import pyinputplus as pin 
 
-
 #TODO: Chuong trinh
-
 def Nhan_thong_tin():
     muc_luong = pin.inputInt('Nhập mức lương: ')
     ca_sang = pin.inputInt('Tổng số ca sáng trong tuần: ')
     ca_toi = pin.inputInt('Tổng số ca tối trong tuần: ')
     ca_chu_nhat = pin.inputInt('Tổng số ca chủ nhật trong tuần: ')
     return muc_luong, ca_sang, ca_toi, ca_chu_nhat
+
+def Tien_chi_hang_thang():
+    tien_ngan_hang = 9100
+    tien_phu_huynh = 2000
+    tien_hoc_cho_con = 4500
+    tien_hoc_them = 1400
+    tien_tieu_xai = 2000
+    tien_dien_thoai = 500
+    tong_chi = tien_ngan_hang + tien_dien_thoai + tien_hoc_cho_con + tien_hoc_them + tien_phu_huynh + tien_tieu_xai
+    return tong_chi
 
 #TODO: Cac chuc nang cua chuong trinh
 def Tinh_luong_du_dinh(muc_luong, ca_sang, ca_toi, ca_chu_nhat):
@@ -19,7 +27,9 @@ def Tinh_luong_du_dinh(muc_luong, ca_sang, ca_toi, ca_chu_nhat):
 def main():
     muc_luong, ca_sang, ca_toi, ca_chu_nhat = Nhan_thong_tin()
     luong_du_dinh = Tinh_luong_du_dinh(muc_luong, ca_sang, ca_toi, ca_chu_nhat)
+    tien_song = Tien_chi_hang_thang()
     print(f"Múc lương dự định của tháng này là: {luong_du_dinh} ngàn đồng.")
+    print(f"Tiền chi hàng tháng là: {tien_song} ngàn đồng.")
 
 
 #TODO: In ra man hinh
